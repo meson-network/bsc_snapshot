86953d36dfb7a9ae8717ca841ce408884e9d3a67
Bruce Wayne <zhangzhengbo@hotmail.com>
2023-09-15 17:08:34 +0800

    Readme (#1)
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit

--- build.sh
+++ build.sh
@@ -3,19 +3,13 @@ mkdir build
 
 
 echo "Compiling Mac amd64 version"
-DIR="darwin-amd64"  &&  GOOS=darwin GOARCH=amd64 go build -a -o "./build/${DIR}/bsc-data-file-utils" 
+DIR="darwin-amd64"  &&  GOOS=darwin GOARCH=amd64 go build -a -o "./build/bsc_snapshot_darwin" 
 
 echo "Compiling Windows amd64 version"
-DIR="windows-amd64"  &&  GOOS=windows GOARCH=amd64 go build -a -o "./build/${DIR}/bsc-data-file-utils.exe" 
-
-echo "Compiling Windows arm64 version"
-DIR="windows-arm64"  &&  GOOS=windows GOARCH=arm64 go build -a -o "./build/${DIR}/bsc-data-file-utils.exe" 
+DIR="windows-amd64"  &&  GOOS=windows GOARCH=amd64 go build -a -o "./build/bsc_snapshot.exe" 
 
 echo "Compiling Linux amd64 version"
-DIR="linux-amd64"  &&  GOOS=linux GOARCH=amd64 go build -a -o "./build/${DIR}/bsc-data-file-utils" 
-
-echo "Compiling Linux ARM32 version"
-DIR="linux-arm32"  &&  GOOS=linux GOARCH=arm go build -a -o "./build/${DIR}/bsc-data-file-utils" 
+DIR="linux-amd64"  &&  GOOS=linux GOARCH=amd64 go build -a -o "./build/bsc_snapshot_linux_amd64" 
 
 echo "Compiling Linux ARM64 version"
-DIR="linux-arm64"  &&  GOOS=linux GOARCH=arm64 go build -a -o "./build/${DIR}/bsc-data-file-utils" 
+DIR="linux-arm64"  &&  GOOS=linux GOARCH=arm64 go build -a -o "./build/bsc_snapshot_linux_arm64" 
