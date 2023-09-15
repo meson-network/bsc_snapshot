62139322f223ff406d039851b3e13489a02e1e1f
goodboy3 <zhangzhengbo@hotmail.com>
2023-09-15 16:48:07 +0800

    commit

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ mkdir build
 
 
 echo "Compiling Mac amd64 version"
-DIR="darwin-amd64"  &&  GOOS=darwin GOARCH=amd64 go build -a -o "./build/${DIR}/bsc_snapshot" 
+DIR="darwin-amd64"  &&  GOOS=darwin GOARCH=amd64 go build -a -o "./build/bsc_snapshot_darwin" 
 
 echo "Compiling Windows amd64 version"
-DIR="windows-amd64"  &&  GOOS=windows GOARCH=amd64 go build -a -o "./build/${DIR}/bsc_snapshot.exe" 
+DIR="windows-amd64"  &&  GOOS=windows GOARCH=amd64 go build -a -o "./build/bsc_snapshot.exe" 
 
 echo "Compiling Linux amd64 version"
-DIR="linux-amd64"  &&  GOOS=linux GOARCH=amd64 go build -a -o "./build/${DIR}/bsc_snapshot" 
+DIR="linux-amd64"  &&  GOOS=linux GOARCH=amd64 go build -a -o "./build/bsc_snapshot_linux_amd64" 
 
 echo "Compiling Linux ARM64 version"
-DIR="linux-arm64"  &&  GOOS=linux GOARCH=arm64 go build -a -o "./build/${DIR}/bsc_snapshot" 
+DIR="linux-arm64"  &&  GOOS=linux GOARCH=arm64 go build -a -o "./build/bsc_snapshot_linux_arm64" 
